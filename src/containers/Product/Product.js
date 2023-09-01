fabe79ab2506eb21e522fe4240df679afc98c263
Mr.Midrow <jeyjeykey@icloud.com>
2023-09-01 15:46:21 +0300

    new branch

--- src/containers/Product/Product.js
+++ src/containers/Product/Product.js
@@ -1,10 +1,32 @@
 import './productStyle.css';
 import { useParams } from 'react-router-dom';
+import { API_URL } from '../../Constants/Constants';
+import { useState, useEffect } from 'react';
 
 const Product = () => {
   const { id } = useParams();
+  const [isData, setIsData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  
+  useEffect(() => {
+    if (!isLoaded) getProduct();
+  }, [isLoaded]);
 
-  return <p>{`Chosen product is: ${id}`}</p>
+  const getProduct = async function () {
+    try {
+      const response = await fetch(`${API_URL}/products/${id}`);
+      const data = await response.json();
+      setIsData(data)
+    } catch (error) {
+      console.log(error)
+    }
+    setIsLoaded(true)
+  };
+  console.log(isData)
+  
+  return (
+    <></>
+  )
 }
 
 export default Product;
